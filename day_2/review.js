c3e340025ccf5e1c0292c4b74a98dd0fdb51013e
Jacob Tran <jacobtran@gmail.com>
2018-02-21 20:25:56 -0800

    add day 2 files

---
+++ day_2/review.js
@@ -0,0 +1,174 @@
+// good evening everyone
+// javascript
+// ECMAScript 6.0
+// created by brendan eich
+// 10 days to create the javascript language
+
+// create dynamic webpages
+// 2018 - robotics, front end web development
+// server side development
+// unity - 3d game engine, vr development
+// numbers
+console.log(5 + 5);
+console.log(10 - 2);
+console.log(9 / 3);
+console.log(13 % 4);
+// 4 * 4;
+// 16;
+// remainder, modulo, modulus
+
+// text
+// text == strings
+// "J"-"a"-"c"-"o"-"b"
+"Jacob";
+'Jacob';
+`Jacob`; // above the tab key
+
+"Jacob's Class";
+'"Hello", he said.';
+`Jacob
+likes
+green
+eggs
+and
+ham.`;
+
+// remembering
+// My name is Jacob Tran and I am 39 years old.
+
+console.log("My name is Jacob Tran and I am 39 years old.");
+
+"Jacob Tran";
+39;
+
+"My name is " + "Jacob Tran" + " and I am " + 39 + " years old.";
+
+// remember
+// variables
+let name = "Tam Kbeili";
+let age = 35;
+
+console.log("My name is "  + name);
+console.log("My age is "  + age);
+
+console.log("My name is " + name + " and I am " + age + " years old.");
+
+// logic gating
+console.log("wake up");
+console.log("brush your teeth");
+
+// console.log("eat breakfast");
+// logic gating
+// always close the barn door
+
+// boolean: true or false
+// true == 1
+// false == 0
+
+// operand
+// 1 == 2; // false
+// 1 == 1; // true
+
+if (false) {
+  console.log("eat breakfast");
+} else {
+  console.log("drink some juice");
+}
+
+console.log("catch the bus");
+console.log("code");
+
+// variables with logic gating
+let age = 6;
+
+if (age > 20) {
+  console.log("I can legally drive.");
+} else if (age > 16) {
+  console.log("I can drive with a guardian");
+} else {
+  console.log("I am too young to drive.");
+}
+
+let name = "Jacob Tran";
+let canDrive = true;
+
+console.log("My name is " + name + ". Can I drive? " + canDrive);
+
+// loops
+// combine variables with looping
+let counter = 0;
+// counter = 1;
+// counter = 2;
+// counter = 3;
+
+// 1 = 0 + 1;
+// 2 = 1 + 1;
+// 3 = 2 + 1;
+
+while (counter < 3) {
+  console.log("Hello!");
+  counter = counter + 1;
+  // break;
+}
+
+// combine variables with looping
+//let counter = 0;
+
+// print out the numbers from 0 to 100
+console.log(0);
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
+console.log(11);
+console.log(12);
+console.log(13); // brute force
+
+let counter = 0;
+counter = counter + 1; // 1
+counter = counter + 1; // 2
+counter = counter + 1; // 3
+counter = counter + 1; // 4
+counter = counter + 1; // 5
+
+while(counter <= 10000) {
+  console.log( counter );
+  // counter = counter + 1;
+  // counter += 1;
+  // post increment or pre increment
+  // ++counter or counter++
+  counter++;
+}
+
+// // initialization
+// let counter = 0;
+
+// // condition
+// while(counter <= 10000) {
+//   console.log( counter );
+//   // increment
+//   counter++;
+// }
+
+// for (init; condition; increment) {
+for (let counter = 0; counter <= 10000; counter++) {
+  console.log( counter );
+}
+
+
+
+
+//
+
+
+// we can get 10 by using a website
+
+
+
+// html css - tim berners lee
