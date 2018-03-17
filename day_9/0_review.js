cec498e50f67ecbd28357fb6a2647d7f58f20724
Jacob Tran <jacobtran@gmail.com>
2018-03-17 15:18:11 -0700

    add day 9 files

---
+++ day_9/0_review.js
@@ -0,0 +1,52 @@
+// good morning everyone!
+
+// 1. data
+
+// text, numbers, boolean, array, object
+"Jacob";
+'Tran';
+`Thang`;
+
+3.14159;
+42;
+24601;
+
+// booleans
+true; // 1
+false; // 0
+// binary Data
+// hole was punched or not
+// the circuit was open or closed
+// the switch was on or off
+
+// data structures
+// arrays, objects
+let switches = [true, true, false, true];
+// 1101
+let names = ["Jacob", "Tran"];
+let shoppingList = ["computer", "phone", "mouse"];
+let machine = [
+  [true, true, false, true],
+  [true, true, false, true],
+  [true, true, false, true],
+  [true, true, false, true],
+  [true, true, false, true]
+];
+
+// objects
+const student = {
+  grade: 95,
+  attendance: 100,
+  extraCurricular: true,
+  name: "Clarity",
+  id: 24601,
+  phone: "(555) 555-5555",
+  email: "claire@codecore.ca",
+  outstandingFees: 19.99
+};
+
+// Databases
+// https://docs.google.com/spreadsheets/d/1D1TRE8bMMJr9jog-Th-DIvyXmVtFrzEw7SCY6N_NfUA/edit?usp=sharing
+
+// 2. browser interaction
+// 3. the web and beyond
